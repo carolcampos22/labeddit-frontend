2c997b5f7f3b739c50f3ccbfa0e768e96ad797a2
carolcampos22 <mendes.carolina2209@gmail.com>
2023-10-11 21:37:42 -0300

    Inputs de login e signup controlados

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,9 +4,20 @@ import './StylesLogin.jsx'
 import { Button, ContainerLogin, CreateAccountButton, InputsLogin, Logo } from './StylesLogin.jsx'
 import { useNavigate } from 'react-router-dom'
 import { goToSignUpPage } from '../../router/coordinator'
+import { useForm } from "../../hooks/useForm"
 
 export const Login = () => {
     const navigate = useNavigate()
+    const [form, onChangeInputs, clearInputs] = useForm({
+        email: "",
+        password: ""
+    })
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        clearInputs()
+
+    }
     return (
         <ContainerLogin>
             <Logo>
@@ -14,12 +25,31 @@ export const Login = () => {
                 <p>O projeto de rede social da Labenu</p>
             </Logo>
             <InputsLogin>
-                <input placeholder='E-mail'></input>
-                <br/>
-                <input placeholder='Senha'></input>
+                <form onSubmit={onSubmit}>
+                <input 
+                    type='email'
+                    value={form.email}
+                    name='email'
+                    onChange={onChangeInputs} 
+                    title='Insira um e-mail válido' 
+                    placeholder='E-mail'
+                    required
+                ></input>
+                <br />
+                <input 
+                    placeholder='Senha'
+                    type='password'
+                    name='password'
+                    value={form.password}
+                    onChange={onChangeInputs}
+                    minLength={6}
+                    required
+                                    
+                ></input>
+                </form>
             </InputsLogin>
             <div>
-                <Button>Continuar</Button>
+                <Button type='submit' onClick={onSubmit}>Continuar</Button>
                 <CreateAccountButton onClick={() => goToSignUpPage(navigate)}>Criar uma conta!</CreateAccountButton>
             </div>
         </ContainerLogin>
@@ -28,3 +58,4 @@ export const Login = () => {
 }
 
 export default Login
+
--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,20 +1,53 @@
 import React from 'react'
 import { Button, InputsSignUpContainer, SignUpContainer, Texts } from './StylesSignUp'
+import { useForm } from '../../hooks/useForm'
 
 const SignUp = () => {
+  const [form, onChangeInputs, clearInputs] = useForm({
+    name: "",
+    email: "",
+    password: ""
+})
+
+const onSubmit = (event) => {
+    event.preventDefault()
+    clearInputs()
+    console.log(form)
+
+}
   return (
     <SignUpContainer>
         <h1>Olá, boas vindas ao LabEddit ;)</h1>
         <InputsSignUpContainer>
-            <input type="text" placeholder='Apelido'/>
-            <input type="email" placeholder='E-mail'/>
-            <input type="password" placeholder='Senha'/>
+            <input 
+              type="text" 
+              name='name'
+              value={form.name}
+              onChange={onChangeInputs}
+              placeholder='Apelido'
+              required
+              />
+            <input 
+              type="email" 
+              name='email'
+              value={form.email}
+              onChange={onChangeInputs}
+              placeholder='E-mail'
+              required
+              />
+            <input 
+              type="password" 
+              name='password'
+              placeholder='Senha'
+              value={form.password}
+              onChange={onChangeInputs}
+              required/>
         </InputsSignUpContainer>
         <Texts>
             <p>Ao continuar, você concorda com o nosso <a href='#'>Contrato de Usuário</a> e nossa <a href='#'>Política de Privacidade</a></p><br/>
             <input type='checkbox'></input> <span>Eu concordo em receber e-mails sobre coisas legais no LabEddit</span>
         </Texts>
-        <Button>Cadastrar</Button>
+        <Button onClick={onSubmit}>Cadastrar</Button>
     </SignUpContainer>
   )
 }
